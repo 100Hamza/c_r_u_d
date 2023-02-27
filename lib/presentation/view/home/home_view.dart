cf2cf7ea2fe2880f5738be0f98bc16cda508f283
AsadKhan261 <asadkhanasii009@gmail.com>
2023-02-27 15:59:34 +0500

    fetch data from firebase

--- lib/presentation/view/home/home_view.dart
+++ lib/presentation/view/home/home_view.dart
@@ -1,26 +1,113 @@
+import 'package:c_r_u_d/presentation/elements/customtext.dart';
+import 'package:c_r_u_d/presentation/elements/navigation_dialog.dart';
 import 'package:c_r_u_d/presentation/view/notes/notes_view.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../../../helper/navigation_helper.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+List<DocumentSnapshot> allData = [];
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('this is init');
+    fetchData();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('this is build');
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          InkWell(
-            child: Icon(Icons.add),
-            onTap: (){
-              NavigationHelper.pushRoute(context, const NoteView());
-            },
-          )
-        ],
-        centerTitle: true,
-        title: Text('Home'),),
-      body: Text("This is Home Screen"),
-    );
+        appBar: AppBar(
+          actions: [
+            InkWell(
+              child: Icon(Icons.add),
+              onTap: () {
+                NavigationHelper.pushRoute(context, const NoteView());
+              },
+            )
+          ],
+          centerTitle: true,
+          title: Text('Home'),
+        ),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: allData.isEmpty
+              ? Center(child: Text('No Data Found'))
+              : ListView.builder(
+                  itemCount: allData.length,
+                  itemBuilder: (context, i) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                CustomText(
+                                  text: allData[i]['Title'],
+                                  size: 18,
+                                ),
+                                CustomText(
+                                  text: allData[i]['Message'],
+                                  size: 13,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                InkWell(
+                                    onTap: () {
+                                      showNavigationDialog(context,
+                                          message:
+                                              'Are You sure you want to delete!',
+                                          buttonText: 'Yes', navigation: () {
+                                        FirebaseFirestore.instance
+                                            .collection('Notes')
+                                            .doc(allData[i]['docID'])
+                                            .delete()
+                                            .then((value) {
+                                          Navigator.pop(context);
+                                        });
+                                      },
+                                          secondButtonText: 'No',
+                                          showSecondButton: true);
+                                    },
+                                    child: const Icon(Icons.delete)),
+                                Icon(Icons.edit),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+        ));
+  }
+
+  fetchData() async {
+    await FirebaseFirestore.instance
+        .collection('Notes')
+        .snapshots()
+        .listen((QuerySnapshot snapshot) {
+      allData.clear();
+      snapshot.docs.forEach((element) {
+        allData.add(element);
+        setState(() {});
+      });
+    });
   }
 }
--- lib/presentation/view/notes/layout/widgets/notes_body.dart
+++ lib/presentation/view/notes/layout/widgets/notes_body.dart
@@ -1,12 +1,10 @@
-
 import 'package:c_r_u_d/presentation/elements/custom_button.dart';
+import 'package:c_r_u_d/presentation/view/notes/layout/widgets/text_field.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:c_r_u_d/presentation/view/notes/layout/widgets/text_field.dart';
-
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:loading_overlay/loading_overlay.dart';
-
 
 class NotesBody extends StatefulWidget {
   const NotesBody({Key? key}) : super(key: key);
@@ -19,53 +17,70 @@ class _NotesBodyState extends State<NotesBody> {
   bool isLoading = false;
   TextEditingController titleController = TextEditingController();
   TextEditingController messageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    print(FirebaseAuth.instance.currentUser!.uid);
     return LoadingOverlay(
       isLoading: isLoading,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20 , vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         child: Column(
           children: [
-            Custom_TextField(label: "Title", hint: "Enter title of the note", controller: titleController),
-            const SizedBox(height: 10,),
-            Custom_TextField(label: "Message", hint: "Enter Notes Message", maxLines: 10, controller: messageController),
-            CustomButton(buttonName: 'Add Notes', onPress: (){
-              _addNotes();
-            },)
+            Custom_TextField(
+                label: "Title",
+                hint: "Enter title of the note",
+                controller: titleController),
+            const SizedBox(
+              height: 10,
+            ),
+            Custom_TextField(
+                label: "Message",
+                hint: "Enter Notes Message",
+                maxLines: 10,
+                controller: messageController),
+            CustomButton(
+              buttonName: 'Add Notes',
+              onPress: () {
+                _addNotes();
+              },
+            )
           ],
         ),
       ),
     );
   }
 
-  _addNotes() async{
+  _addNotes() async {
     isLoadingTrue();
     print("Loading is True");
     var id = FirebaseAuth.instance.currentUser!.uid;
-      print("User ID: $id");
-    await FirebaseFirestore.instance.collection('Users ID').doc(id).collection("Notes").doc().set({
+    var docID = FirebaseFirestore.instance.collection('Notes').doc().id;
+    await FirebaseFirestore.instance.collection('Notes').doc(docID).set({
       "Title": titleController.text,
       "Message": messageController.text,
+      "userId": id,
+      "docID": docID,
     }).whenComplete(() {
       isLoadingFalse();
-      print("Loading is False ");
+      titleController.clear();
+      messageController.clear();
+      Fluttertoast.showToast(msg: 'Notes Upload Successfully');
+      Navigator.pop(context);
+
       // NavigationHelper.pushRoute(context, const Home());
     });
   }
 
-  void isLoadingTrue()
-  {
+  void isLoadingTrue() {
     setState(() {
       isLoading = true;
     });
   }
 
-  void isLoadingFalse()
-  {
+  void isLoadingFalse() {
     setState(() {
       isLoading = false;
     });
   }
-
 }
